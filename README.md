951a0df238d8e441008f098178f5366882a6e3d0
Bastian Schwarz <bastian@codename-php.de>
2017-09-12 20:40:40 +0200

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef.cookbook.docker
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef.cookbook.docker
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
