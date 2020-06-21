983d7c1a6b8b71ceaf6ab2db71a1d7e3cb6446a6
Bastian Schwarz <bastian@codename-php.de>
2020-06-22 00:03:35 +0200

    Bumped version to 2.0.0 since docker compose was removed

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer 'Bastian Schwarz'
 maintainer_email 'bastian@codename-php.de'
 license 'Apache-2.0'
 description 'Installs/Configures codenamephp_docker'
-version '1.1.0'
+version '2.0.0'
 chef_version '>= 13.0' if respond_to?(:chef_version)
 issues_url 'https://github.com/codenamephp/chef.cookbook.docker/issues'
 source_url 'https://github.com/codenamephp/chef.cookbook.docker'
