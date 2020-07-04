5ee0d5dd2cb46dfd94c76302d3cb5fbff1188165
Bastian Schwarz <bastian@codename-php.de>
2020-07-04 18:47:56 +0200

    Added tasks config for vscode

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ maintainer 'Bastian Schwarz'
 maintainer_email 'bastian@codename-php.de'
 license 'Apache-2.0'
 description 'Installs/Configures codenamephp_docker'
-version '2.0.0'
-chef_version '>= 13.0' if respond_to?(:chef_version)
+version '3.0.0'
+chef_version '>= 13.0'
 issues_url 'https://github.com/codenamephp/chef.cookbook.docker/issues'
 source_url 'https://github.com/codenamephp/chef.cookbook.docker'
 
