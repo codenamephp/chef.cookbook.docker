b47ce72aec91c8c0b8d29e37e31435b41dd05d3e
Bastian Schwarz <bastian@codename-php.de>
2020-07-04 20:21:39 +0200

    Trying to add resolver cookbook with nameserver to see if github.com can then be resolved

--- test/fixtures/cookbooks/test/metadata.rb
+++ test/fixtures/cookbooks/test/metadata.rb
@@ -10,3 +10,4 @@ version '1.0.0'
 supports 'debian', '~>10'
 
 depends 'codenamephp_docker'
+depends 'resolver'
--- test/fixtures/cookbooks/test/recipes/default.rb
+++ test/fixtures/cookbooks/test/recipes/default.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+include_recipe 'resolver'
 codenamephp_docker_service 'Install docker'
 
 codenamephp_docker_compose 'Install docker-compose'
