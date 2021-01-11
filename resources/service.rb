7a10a91494d87fae3580078c5071a408ad8607fa
Bastian Schwarz <bastian@codename-php.de>
2021-01-11 14:01:27 +0100

    Added dns property to docker_service resource since pulling the images sometimes fails and several github issues point to setting google dns as fix

--- resources/service.rb
+++ resources/service.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
+property :dns, [String, Array], default: ['8.8.8.8', '8.8.4.4'], description: 'DNS settings to pass to the service configuration'
 
 action :install do
   docker_service 'default' do
     action %i(create start)
+    dns new_resource.dns
   end
 end
--- spec/unit/resources/service_spec.rb
+++ spec/unit/resources/service_spec.rb
@@ -27,4 +27,21 @@ describe 'codenamephp_docker_service' do
       expect(chef_run).to start_docker_service('default')
     end
   end
+
+  context 'With custom dns' do
+    recipe do
+      codenamephp_docker_service 'Install docker' do
+        dns %w(some dns)
+      end
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+
+    it 'creates and starts docker service using resource' do
+      expect(chef_run).to create_docker_service('default').with(dns: %w(some dns))
+      expect(chef_run).to start_docker_service('default')
+    end
+  end
 end
